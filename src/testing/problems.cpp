13a195275f5a2e4daabc1f31ffac6964109dfc47
semiexp <linthya.v@gmail.com>
2014-08-20 11:55:31 +0900

    Add a test problem

--- src/testing/problems.cpp
+++ src/testing/problems.cpp
@@ -6,7 +6,7 @@ Format for each room: (top y, top x, height, width, hint)
  */
 
 /* 
-Source: http://www.nikoli.com/ja/puzzles/heyawake/rule.html 
+Source: http://www.nikoli.com/en/puzzles/heyawake/rule.html
  */
 
 int problem1[][5] = 
@@ -35,6 +35,31 @@ int problem1[][5] =
 	{ 8, 8, 2, 2, -1 }
 };
 
+/*
+Source: http://www.nikoli.com/en/puzzles/heyawake/ Sample 1
+*/
+
+int problem2[][5] = 
+{
+	{ 0, 0, 2, 2, -1 },
+	{ 0, 2, 2, 3, -1 },
+	{ 0, 5, 2, 3, -1 },
+	{ 0, 8, 2, 2, 0 },
+	{ 2, 0, 3, 2, -1 },
+	{ 2, 2, 3, 3, 5 },
+	{ 2, 5, 1, 3, -1 },
+	{ 2, 8, 3, 2, 1 },
+	{ 3, 5, 2, 3, 0 },
+	{ 5, 0, 3, 2, 1 },
+	{ 5, 2, 2, 3, 2 },
+	{ 5, 5, 3, 3, 2 },
+	{ 5, 8, 3, 2, -1 },
+	{ 7, 2, 1, 3, -1 },
+	{ 8, 0, 2, 2, 0 },
+	{ 8, 2, 2, 3, -1 },
+	{ 8, 5, 2, 3, 1 },
+	{ 8, 8, 2, 2, -1 }
+};
 
 HYProblem GetTestProblem(int pid)
 {
@@ -47,6 +72,11 @@ HYProblem GetTestProblem(int pid)
 		src = problem1;
 		len = sizeof(problem1) / (5 * sizeof(int));
 		break; 
+	case 1:
+		ret = HYProblem(10, 10);
+		src = problem2;
+		len = sizeof(problem2) / (5 * sizeof(int));
+		break;
 	}
 
 	for (int i = 0; i < len; i++) {
