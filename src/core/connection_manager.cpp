e66ac6f3aac1f1bf44f02c465f5ba9f4749e249b
semiexp <linthya.v@gmail.com>
2014-09-08 22:51:28 +0900

    Improve the performance of the connection manager

--- src/core/connection_manager.cpp
+++ src/core/connection_manager.cpp
@@ -25,7 +25,7 @@ bool HYConnectionManager::CheckValidity(CellCord y, CellCord x)
 	for (int i = 0; i < 4; i++) {
 		int y2 = y + HYField::dy[i] + HYField::dy[(i + 3) % 4], x2 = x + HYField::dx[i] + HYField::dx[(i + 3) % 4];
 
-		ids[i] = Range(y2, x2) ? Id(y2, x2) : aux;
+		ids[i] = Root(Range(y2, x2) ? Id(y2, x2) : aux);
 	}
 
 	ids[4] = ids[0];
@@ -34,17 +34,13 @@ bool HYConnectionManager::CheckValidity(CellCord y, CellCord x)
 		int y2 = y + HYField::dy[i], x2 = x + HYField::dx[i];
 
 		if (Range(y2, x2)) {
-			if (ids[i] != -1 && ids[i + 1] != -1) {
-				if (Root(ids[i]) == Root(ids[i + 1])) {
-					return false;
-				}
-			}
+			if (ids[i] == ids[i + 1]) return false;
 		}
 	}
 
 	if (0 < y && y < height - 1 && 0 < x && x < width - 1) {
-		if (ids[0] != -1 && ids[2] != -1 && Root(ids[0]) == Root(ids[2])) return false;
-		if (ids[1] != -1 && ids[3] != -1 && Root(ids[1]) == Root(ids[3])) return false;
+		if (Root(ids[0]) == Root(ids[2])) return false;
+		if (Root(ids[1]) == Root(ids[3])) return false;
 	}
 
 	return true;
