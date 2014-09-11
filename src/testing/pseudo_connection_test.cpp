59d419d7a1478930e2247232da7ce82774315900
semiexp <linthya.v@gmail.com>
2014-09-11 18:33:39 +0900

    Change pseudo connection tester

--- src/testing/pseudo_connection_test.cpp
+++ src/testing/pseudo_connection_test.cpp
@@ -3,14 +3,17 @@
 
 void PseudoConnectionTest()
 {
+	HYRoomDatabase::Initialize();
+
 	HYProblem prob(10, 10);
 	prob.AddHint(0, 0, 3, 2, -1);
 	prob.AddHint(0, 2, 3, 1, 2);
 	prob.AddHint(0, 3, 10, 7, -1);
-	prob.AddHint(3, 0, 7, 3, -1);
+	prob.AddHint(3, 0, 2, 2, 2);
+	prob.AddHint(3, 2, 7, 1, -1);
+	prob.AddHint(5, 0, 5, 3, -1);
 
 	HYField sol(prob);
-	sol.DetermineBlack(3, 1);
 
 	HYSolver::Solve(sol);
 	sol.Debug();
